33482f230147678d7b7e9ad2c10389ab684f1d40
Maoz  Nachum <maoz123000@gmail.com>
2026-01-11 15:59:57 +0200

    שינויים עדכני 16:00

--- my-vet/src/main.jsx
+++ my-vet/src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+// יצירת ערכת נושא בסיסית (אפשר לשנות צבעים כאן בעתיד)
+const theme = createTheme({
+  palette: {
+    background: {
+      default: "#f4f6f8" // צבע רקע אפור בהיר-מקצועי לכל האפליקציה
+    }
+  }
+});
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline /> {/* זה הרכיב שמסדר את העיצוב הבסיסי */}
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>,
 )
